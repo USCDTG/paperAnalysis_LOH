fd978976ee73cdb25c8288c7a1588d81e75a7f7c
Michelle Webb <31837356+michelgw@users.noreply.github.com>
2023-09-02 00:27:44 -0700

    add paper figure R scripts

---
+++ Figure2_paper.R
@@ -0,0 +1,100 @@
+# Michelle Webb
+# Figure 2 Analysis Workflow
+# June 15, 2023
+
+library(ggpubr) # version 0.6.0
+library(GenomicRanges) # version 1.50.2
+library(IRanges) # version 2.32.0
+library(VariantAnnotation) # version 1.44.1
+library(regioneR) # 1.30.0
+
+# Figure 2A
+# Diagram
+
+# Figure 2B 
+# IGV Image
+
+# Figure 2C
+options(scipen=999)
+dataToUse <- read.csv('~/allSamples_tLOH.csv')
+imported2 <- dataToUse %>% group_by(chromosome) %>%
+    distinct(chromosome,position)
+snps <- imported2[!(imported2$chromosome == 6
+                    & imported2$position > 28510120 
+                    & imported2$position < 33500500),]
+snps$seqnames <- paste0('chr',snps$chromosome)
+snps$chromosome <- NULL
+snps$start <- snps$position
+snps$end <- snps$position
+snps$position <- NULL
+snps$CHR <- gsub('^','chr',snps$chromosome)
+snps <- snps %>% filter(seqnames !='chrX' & seqnames !='chrY')
+rangedSNPS <- regioneR::toGRanges(A = data.frame(snps))
+
+
+kp <- karyoploteR::plotKaryotype(genome="hg19")
+kp <- karyoploteR::kpPlotDensity(kp, data=rangedSNPS, col="blue", window.size = 10000)
+max.density <- kp$latest.plot$computed.values$max.density
+# karyoploteR::kpAxis(kp, ymin=0, ymax=max.density, numticks = 3,cex =0.5)
+
+png(filename = '~/Karyoplote.png', width = 520, height = 520)
+pp <- karyoploteR::getDefaultPlotParams(plot.type=1)
+pp$margin <- 0.1
+pp$bottommargin <- 150
+pp$data1height <- 500
+pp$ideogramheight <- 50
+kp <- karyoploteR::plotKaryotype(plot.type=1, genome='hg19',cex=1.85, plot.params = pp, chromosomes = c('chr1','chr2','chr3','chr4','chr5',
+                                                                                                        'chr6','chr7','chr8','chr9','chr10',
+                                                                                                        'chr11','chr12','chr13','chr14','chr15',
+                                                                                                        'chr17','chr18','chr19','chr20','chr21',
+                                                                                                        'chr22'))
+kp <- karyoploteR::kpPlotDensity(kp, data=rangedSNPS, col = 'blue')
+dev.off()
+# Figure 2D
+# Diagram
+
+# Figure 2E
+a1 <- ggplot(dataToUse,aes(x=segment_lengthOfInterval)) + geom_histogram(color = 'darkslategray4', fill = 'darkslategray4') +
+    scale_x_continuous(labels = scales::unit_format(unit = "M", scale = 1e-6),expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    xlab('Segment Length') +
+    theme(axis.text.y = element_text(size = 15, family = 'Arial Narrow', face = 'bold'),
+          axis.line = element_line(color='black'),
+          axis.title.x = element_text(size = 15, family = 'Arial Narrow', face = 'bold'),
+          axis.text.x = element_text(size = 15, family = 'Arial Narrow', face = 'bold'),
+          axis.title.y = element_text(size = 15, family = 'Arial Narrow', face = 'bold'))
+
+b1 <- ggplot(dataToUse,aes(x=segment_nSNPs)) + geom_histogram(color = 'darkslategray4', fill = 'darkslategray4') +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    xlab('# of SNPs per Segment') +
+    theme(axis.text.y = element_text(size = 15, family = 'Arial Narrow', face = 'bold'),
+          axis.line = element_line(color='black'),
+          axis.title.x = element_text(size = 15, family = 'Arial Narrow', face = 'bold'),
+          axis.text.x = element_text(size = 15, family = 'Arial Narrow', face = 'bold'),
+          axis.title.y = element_text(size = 15, family = 'Arial Narrow', face = 'bold'))
+
+c1 <- ggplot(dataToUse,aes(x=segment_modePeak)) + geom_histogram(color = 'darkslategray4', fill = 'darkslategray4') +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    xlab('Segment Mode Peak') +
+    theme(axis.text.y = element_text(size = 15, family = 'Arial Narrow', face = 'bold'),
+          axis.line = element_line(color='black'),
+          axis.title.x = element_text(size = 15, family = 'Arial Narrow', face = 'bold'),
+          axis.text.x = element_text(size = 15, family = 'Arial Narrow', face = 'bold'),
+          axis.title.y = element_text(size = 15, family = 'Arial Narrow', face = 'bold'))
+d1 <- ggplot(dataToUse,aes(x=segment_sequentialSum)) + geom_histogram(color = 'darkslategray4', fill = 'darkslategray4') +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_y_continuous(expand = c(0, 0)) +
+    xlab('Segment Sequential Sum Values') +
+    theme(axis.text.y = element_text(size = 15, family = 'Arial Narrow', face = 'bold'),
+          axis.line = element_line(color='black'),
+          axis.title.x = element_text(size = 15, family = 'Arial Narrow', face = 'bold'),
+          axis.text.x = element_text(size = 15, family = 'Arial Narrow', face = 'bold'),
+          axis.title.y = element_text(size = 15, family = 'Arial Narrow', face = 'bold'))
+
+m <- ggpubr::ggarrange(a1,b1,c1,d1,
+                       labels = c("i","ii",'iii','iv'),
+                       ncol=2,
+                       nrow=2)
+ggsave(m,filename = '~/figure2_histograms.png')
---
+++ Figure4_paper.R
@@ -0,0 +1,111 @@
+# Michelle Webb
+# Figure 4
+# Spatial Transcriptomics ESTIMATE Analysis
+# June 15, 2023
+
+library(Seurat) # version 4.3.0
+library(ggplot2) # version 3.4.2
+library(dplyr) # version 1.1.2
+library(tLOH) # version 1.6.0
+
+# Variables
+distinctColors <- c("dodgerblue2", "#E31A1C","green4","#6A3D9A","#FF7F00","black", "gold1","skyblue2",
+                    "#FF00FF","palegreen2","darkturquoise","brown","#CAB2D6","#FDBF6F")
+
+# Figure 4A
+seurat <- Load10X_Spatial(data.dir = '~/DiffuseMidlineGlioma7/outs')
+cluster <- read.csv('~/DiffuseMidlineGlioma7/outs/analysis/clustering/graphclust/clusters.csv', 
+                    row.names = 1)
+seurat@meta.data <- transform(merge(seurat@meta.data,cluster,by=0,all=TRUE), row.names=Row.names, Row.names=NULL)
+SpatialDimPlot(seurat, group.by='Cluster', image.alpha=0,stroke=0, pt.size.factor = 0) +
+    theme(legend.key = element_rect(colour = NA, fill = NA),
+          legend.box.background = element_blank(),
+          legend.text = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+          legend.title = element_text(size = 20, family = 'Arial Narrow', face = 'bold')) +
+    guides(fill = guide_legend(override.aes = list(size=5))) +
+    labs(fill='Cluster')
+
+# Figure 4B
+clusterPlot <- SpatialDimPlot(seurat, group.by='Cluster', image.alpha=0,stroke=0) + 
+    scale_fill_manual(values=distinctColors[1:9]) + 
+    theme(legend.key = element_rect(colour = NA, fill = NA),
+          legend.box.background = element_blank(),
+          legend.text = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+          legend.title = element_text(size = 20, family = 'Arial Narrow', face = 'bold')) +
+    guides(fill = guide_legend(override.aes = list(size=5))) +
+    labs(fill='Cluster')
+ggsave(clusterPlot,filename = '~/diffuseMidlineGlioma7_clusterPlot.png')
+
+
+# Figure 4C
+estimateOutput <- read.csv('~/DiffuseMidlineGlioma7_estimateOutput.csv', row.names = 1)
+seurat@meta.data <- transform(merge(seurat@meta.data,estimateOutput[c('StromalScore','TumorPurity')],by=0,all=TRUE),row.names=Row.names,Row.names=NULL)
+
+tumorPurityPlot <- SpatialFeaturePlot(seurat, stroke=0,features = 'TumorPurity', image.alpha=0) + theme(
+    legend.text = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+    legend.title = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+    legend.position='right')
+ggsave(tumorPurityPlot,
+       filename = '~/diffuseMidlineGlioma7_tumorPurityPlot.png')
+
+# Figure 4D
+stromalScorePlot <- SpatialFeaturePlot(seurat, stroke=0,features = 'StromalScore', image.alpha=0) + theme(
+    legend.text = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+    legend.title = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+    legend.position='right')
+ggsave(stromalScorePlot,
+       filename = '~/diffuseMidlineGlioma7_tumorPurityPlot.png')
+
+# Figure 4E
+pdgfraPlot <- SpatialFeaturePlot(seurat, features = 'PDGFRA', pt.size.factor = 0) + theme(
+    legend.text = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+    legend.title = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+    legend.position='none')
+
+ggsave(pdgfraPlot,filename = '~/diffuseMidlineGlioma7_PDGFRA.png')
+
+# Figure 4F
+minidf <- seurat@meta.data
+colors <- distinctColors[1:length(unique(minidf$Cluster))]
+boxplot <- ggplot(minidf,aes(x=as.factor(Cluster),y=StromalScore,color=as.factor(Cluster))) + 
+    geom_boxplot() + 
+    scale_color_manual(values = colors) +
+    theme(axis.text.y = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+          axis.line = element_line(color='black'),
+          axis.title.x = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+          axis.text.x = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+          axis.title.y = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+          legend.text = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+          legend.title = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+          legend.position='none') +
+    xlab('Cluster') +
+    ylab('Stromal Score')
+ggsave(boxplot,filename = '~/diffuseMidlineGlioma7_stromalScore_boxplot.png')
+
+# Figure 4G
+tlohPlot <- ggplot(toPlot,aes(x=position,
+                        y=alleleFraction,
+                        color=segment_State)) + 
+    geom_point(size = 0.25) +
+    facet_grid(cluster~chromosome, switch = 'x', scales = 'free') +
+    scale_color_manual(values = c('blue','goldenrod'), labels = c('Heterozygous','LOH')) +
+    theme(strip.text = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+          axis.title.x = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+          axis.title.y = element_text(size = 20, family = 'Arial Narrow', face = 'bold'),
+          legend.position = 'bottom',
+          legend.background = element_rect(color = NA),
+          legend.key = element_rect(colour = NA, fill = NA),
+          legend.box.background = element_blank(),
+          panel.spacing = unit(0.00000001, 'lines'),
+          panel.grid = element_blank(),
+          panel.background = element_rect(fill = NA, color = NA),
+          axis.ticks.x = element_blank(),
+          axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.y = element_blank(),
+          aspect.ratio = 1) +
+    guides(colour = guide_legend(override.aes = list(size=3))) +
+    labs(color='State') +
+    xlab('Chromosome') +
+    ylab('Allele Fraction')
+ggsave(tlohPlot, filename = '~/diffuseMidlineGlioma7_paper_tLOH.png')
