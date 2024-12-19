fb91da62ab6a76d1e4e1d5ad4f91ac18ed90ed0e
Michelle Webb <31837356+michelgw@users.noreply.github.com>
2024-12-18 18:41:22 -0800

    Update Supplementary Figure 5 and 6

--- figureScripts/SupplementaryFigure5.R
+++ figureScripts/SupplementaryFigure5.R
@@ -1,30 +1,87 @@
 library(dplyr) # version 1.1.4  
 library(purrr) #  version 1.0.2
 library(Seurat) # version 5.1.0
+library(viridis) # version 0.6.5
+library(ggplot2) # version 3.5.1
 
+GlioblastomaA3 <- Load10X_Spatial('~/GlioblastomaA3/outs/')
+GlioblastomaA5 <- Load10X_Spatial('~/GlioblastomaA5/outs/')
+OligodendrogliomaA3 <- Load10X_Spatial('~/OligodendrogliomaA3/outs/')
 
-genes <- c('VEGFA','PDGFRA','HIF1A')
-samples <- c('Glioblastoma A4','Glioblastoma A5','Oligodendroglioma A2')
+GlioblastomaA3 <- SCTransform(GlioblastomaA3, assay = "Spatial", verbose = FALSE)
+a <- SpatialFeaturePlot(GlioblastomaA3,features='VEGFA',image.alpha = 0, pt.size.factor = 2.6) + 
+    theme(legend.position='right',
+          legend.title = element_text(face='bold.italic',family='Arial Narrow',size=15),
+          legend.text = element_text(face='bold',family='Arial Narrow',size=15)) +
+    scale_fill_viridis(option='magma')
 
+ggsave(a, filename = 'SupplementaryFigure5_GlioblastomaA3_VEGFA.png',height=7,width=7)
 
-GlioblastomaA3 <- Load10X_Spatial('~/Google Drive/Shared drives/10X_Alpha_Test_Phase_1_and_2/manuscript_revision3/spatialData/GlioblastomaA3 (1)/outs/')
-GlioblastomaA5 <- Load10X_Spatial('~/Google Drive/Shared drives/10X_Alpha_Test_Phase_1_and_2/manuscript_revision3/spatialData/GlioblastomaA5/outs/')
-OligodendrogliomaA3 <- Load10X_Spatial('~/Google Drive/Shared drives/10X_Alpha_Test_Phase_1_and_2/manuscript_revision3/spatialData/OligodendrogliomaA3/outs/')
+b <- SpatialFeaturePlot(GlioblastomaA3,features='PDGFRA',image.alpha = 0, pt.size.factor = 2.6) + 
+    theme(legend.position='right',
+          legend.title = element_text(face='bold.italic',family='Arial Narrow',size=15),
+          legend.text = element_text(face='bold',family='Arial Narrow',size=15)) +
+    scale_fill_viridis(option='magma')
 
-GlioblastomaA3 <- NormalizeData(GlioblastomaA3)
-SpatialFeaturePlot(GlioblastomaA3,features='VEGFA',image.alpha = 0, pt.size.factor = 2.3) + theme(legend.position = 'right')
-SpatialFeaturePlot(GlioblastomaA3,features='PDGFRA',image.alpha = 0, pt.size.factor = 2.3) + theme(legend.position = 'right')
-SpatialFeaturePlot(GlioblastomaA3,features='HIF1A',image.alpha = 0, pt.size.factor = 2.3) + theme(legend.position = 'right')
+ggsave(b, filename = 'SupplementaryFigure5_GlioblastomaA3_PDGFRA.png',height=7,width=7)
 
-GlioblastomaA5 <- NormalizeData(GlioblastomaA5)
-SpatialFeaturePlot(GlioblastomaA5,features='VEGFA',image.alpha = 0, pt.size.factor = 2.3) + theme(legend.position = 'right')
-SpatialFeaturePlot(GlioblastomaA5,features='PDGFRA',image.alpha = 0, pt.size.factor = 2.3) + theme(legend.position = 'right')
-SpatialFeaturePlot(GlioblastomaA5,features='HIF1A',image.alpha = 0, pt.size.factor = 2.3) + theme(legend.position = 'right')
+c <- SpatialFeaturePlot(GlioblastomaA3,features='HIF1A',image.alpha = 0, pt.size.factor = 2.6) + 
+    theme(legend.position='right',
+          legend.title = element_text(face='bold.italic',family='Arial Narrow',size=15),
+          legend.text = element_text(face='bold',family='Arial Narrow',size=15)) +
+    scale_fill_viridis(option='magma')
 
-OligodendrogliomaA3 <- NormalizeData(OligodendrogliomaA3)
-SpatialFeaturePlot(OligodendrogliomaA3,features='VEGFA',image.alpha = 0, pt.size.factor = 2.3) + theme(legend.position = 'right')
-SpatialFeaturePlot(OligodendrogliomaA3,features='PDGFRA',image.alpha = 0, pt.size.factor = 2.3) + theme(legend.position = 'right')
-SpatialFeaturePlot(OligodendrogliomaA3,features='HIF1A',image.alpha = 0, pt.size.factor = 2.3) + theme(legend.position = 'right')
+ggsave(c, filename = 'SupplementaryFigure5_GlioblastomaA3_HIF1A.png',height=7,width=7)
+
+GlioblastomaA5 <- SCTransform(GlioblastomaA5, assay = "Spatial", verbose = FALSE)
+d <- SpatialFeaturePlot(GlioblastomaA5,features='VEGFA',image.alpha = 0, pt.size.factor = 2.6) + 
+    theme(legend.position='right',
+          legend.title = element_text(face='bold.italic',family='Arial Narrow',size=15),
+          legend.text = element_text(face='bold',family='Arial Narrow',size=15)) +
+    scale_fill_viridis(option='magma')
+
+ggsave(d, filename = 'SupplementaryFigure5_GlioblastomaA5_VEGFA.png',height=7,width=7)
+
+e <- SpatialFeaturePlot(GlioblastomaA5,features='PDGFRA',image.alpha = 0, pt.size.factor = 2.6) + 
+    theme(legend.position='right',
+          legend.title = element_text(face='bold.italic',family='Arial Narrow',size=15),
+          legend.text = element_text(face='bold',family='Arial Narrow',size=15)) +
+    scale_fill_viridis(option='magma')
+
+ggsave(e, filename = 'SupplementaryFigure5_GlioblastomaA5_PDGFRA.png',height=7,width=7)
+
+f <- SpatialFeaturePlot(GlioblastomaA5,features='HIF1A',image.alpha = 0, pt.size.factor = 2.6) + 
+    theme(legend.position='right',
+          legend.title = element_text(face='bold.italic',family='Arial Narrow',size=15),
+          legend.text = element_text(face='bold',family='Arial Narrow',size=15)) +
+    scale_fill_viridis(option='magma')
+
+ggsave(f, filename = 'SupplementaryFigure5_GlioblastomaA5_HIF1A.png',height=7,width=7)
+
+OligodendrogliomaA3 <- SCTransform(OligodendrogliomaA3, assay = "Spatial", verbose = FALSE)
+g <- SpatialFeaturePlot(OligodendrogliomaA3,features='VEGFA',image.alpha = 0, pt.size.factor = 2.4) + 
+    theme(legend.position='right',
+          legend.title = element_text(face='bold.italic',family='Arial Narrow',size=15),
+          legend.text = element_text(face='bold',family='Arial Narrow',size=15)) +
+    scale_fill_viridis(option='magma')
+
+ggsave(g, filename = 'SupplementaryFigure5_OligodendrogliomaA3_VEGFA.png',height=7,width=7)
+
+h <- SpatialFeaturePlot(OligodendrogliomaA3,features='PDGFRA',image.alpha = 0, pt.size.factor = 2.4) + 
+    theme(legend.position='right',
+          legend.title = element_text(face='bold.italic',family='Arial Narrow',size=15),
+          legend.text = element_text(face='bold',family='Arial Narrow',size=15)) +
+    scale_fill_viridis(option='magma')
+
+ggsave(h, filename = 'SupplementaryFigure5_OligodendrogliomaA3_PDGFRA.png',height=7,width=7)
+
+i <- SpatialFeaturePlot(OligodendrogliomaA3,features='HIF1A',image.alpha = 0, pt.size.factor = 2.4) + 
+    theme(legend.position='right',
+          legend.title = element_text(face='bold.italic',family='Arial Narrow',size=15),
+          legend.text = element_text(face='bold',family='Arial Narrow',size=15)) +
+    scale_fill_viridis(option='magma')
+
+ggsave(i, filename = 'SupplementaryFigure5_OligodendrogliomaA3_HIF1A.png',height=7,width=7)
 
 
 
--- figureScripts/SupplementaryFigure6.R
+++ figureScripts/SupplementaryFigure6.R
@@ -1,39 +1,46 @@
 library(Seurat) # version 5.1.0
 library(dplyr) # version 1.1.4  
-
-setwd('~Figures')
+library(viridis) # version 0.6.5
+library(ggplot2) # version 3.5.1
 
 GlioblastomaA1 <- Load10X_Spatial('~/GlioblastomaA1/outs/')
-GlioblastomaA1 <- NormalizeData(GlioblastomaA1)
+GlioblastomaA1 <- SCTransform(GlioblastomaA1, assay = "Spatial", verbose = FALSE)
 
-a1 <- SpatialFeaturePlot(GlioblastomaA1, features = 'VEGFA',slot = 'data',image.alpha=0,pt.size.factor = 2.5) +
+a1 <- SpatialFeaturePlot(GlioblastomaA1, features = 'VEGFA',slot = 'data',image.alpha=0,pt.size.factor = 2.6) +
     theme(legend.position='right',
           legend.title = element_text(face='bold.italic',family='Arial Narrow',size=15),
-          legend.text = element_text(face='bold',family='Arial Narrow',size=15))
+          legend.text = element_text(face='bold',family='Arial Narrow',size=15)) +
+    scale_fill_viridis(option='magma')
+
 ggsave(a1, file = 'SupplementaryFigure6_a_VEGFA.png', height = 7, width = 7)
-a2 <- SpatialFeaturePlot(GlioblastomaA1, features = 'HIF1A',slot = 'data',image.alpha=0,pt.size.factor = 2.5) +
+a2 <- SpatialFeaturePlot(GlioblastomaA1, features = 'HIF1A',slot = 'data',image.alpha=0,pt.size.factor = 2.6) +
     theme(legend.position='right',
           legend.title = element_text(face='bold.italic',family='Arial Narrow',size=15),
-          legend.text = element_text(face='bold',family='Arial Narrow',size=15))
+          legend.text = element_text(face='bold',family='Arial Narrow',size=15)) +
+    scale_fill_viridis(option='magma')
 ggsave(a2, file = 'SupplementaryFigure6_b_HIF1A.png', height = 7, width = 7)
-a3 <- SpatialFeaturePlot(GlioblastomaA1, features = 'PDGFRA',slot = 'data',image.alpha=0,pt.size.factor = 2.5) +
+a3 <- SpatialFeaturePlot(GlioblastomaA1, features = 'PDGFRA',slot = 'data',image.alpha=0,pt.size.factor = 2.6) +
     theme(legend.position='right',
           legend.title = element_text(face='bold.italic',family='Arial Narrow',size=15),
-          legend.text = element_text(face='bold',family='Arial Narrow',size=15))
+          legend.text = element_text(face='bold',family='Arial Narrow',size=15)) +
+    scale_fill_viridis(option='magma')
 ggsave(a3, file = 'SupplementaryFigure6_c_PDGFRA.png', height = 7, width = 7)
-a4 <- SpatialFeaturePlot(GlioblastomaA1, features = 'NDRG1',slot = 'data',image.alpha=0,pt.size.factor = 2.5) +
+a4 <- SpatialFeaturePlot(GlioblastomaA1, features = 'NDRG1',slot = 'data',image.alpha=0,pt.size.factor = 2.6) +
     theme(legend.position='right',
           legend.title = element_text(face='bold.italic',family='Arial Narrow',size=15),
-          legend.text = element_text(face='bold',family='Arial Narrow',size=15))
+          legend.text = element_text(face='bold',family='Arial Narrow',size=15)) +
+    scale_fill_viridis(option='magma')
 ggsave(a4, file = 'SupplementaryFigure6_d_NDRG1.png', height = 7, width = 7)
-a5 <- SpatialFeaturePlot(GlioblastomaA1, features = 'DDIT4',slot = 'data',image.alpha=0,pt.size.factor = 2.5) +
+a5 <- SpatialFeaturePlot(GlioblastomaA1, features = 'DDIT4',slot = 'data',image.alpha=0,pt.size.factor = 2.6) +
     theme(legend.position='right',
           legend.title = element_text(face='bold.italic',family='Arial Narrow',size=15),
-          legend.text = element_text(face='bold',family='Arial Narrow',size=15))
+          legend.text = element_text(face='bold',family='Arial Narrow',size=15)) +
+    scale_fill_viridis(option='magma')
 ggsave(a5, file = 'SupplementaryFigure6_e_DDIT4.png', height = 7, width = 7)
-a6 <- SpatialFeaturePlot(GlioblastomaA1, features = 'CD44',slot = 'data',image.alpha=0,pt.size.factor = 2.5) +
+a6 <- SpatialFeaturePlot(GlioblastomaA1, features = 'CD44',slot = 'data',image.alpha=0,pt.size.factor = 2.6) +
     theme(legend.position='right',
           legend.title = element_text(face='bold.italic',family='Arial Narrow',size=15),
-          legend.text = element_text(face='bold',family='Arial Narrow',size=15))
+          legend.text = element_text(face='bold',family='Arial Narrow',size=15)) +
+    scale_fill_viridis(option='magma')
 ggsave(a6, file = 'SupplementaryFigure6_f_CD44.png', height = 7, width = 7)
 
